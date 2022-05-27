fe2ab14bd9281acbb5dde381ef8cab09220e9953
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-05-27 08:51:50 -0300

    Dev: Wallet reducer created

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { actionTypes } from '../redux/actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case actionTypes.SAVE_TASK:
+    return { ...state, task: action.task };
+  case actionTypes.SAVE_EXPENSES:
+    return { ...state, expenses: [...state.expenses, action.payload] };
+  case actionTypes.REMOVE_EXPENSE:
+    return {
+      ...state,
+      expenses: state.expenses.filter(({ id }) => action.payload !== id),
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
