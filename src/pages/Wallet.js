95da6e6924372dea173cb2ce7ded7a1308a96a23
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-05-27 09:26:55 -0300

    Dev: Include expensesTable in wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Expenses from '../components/Expenses';
+import ExpensesTable from '../components/ExpensesTable';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <Expenses />
+        <ExpensesTable />
       </>);
   }
 }
