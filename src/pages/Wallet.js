0b9d90948f0608f2dacabe8b4731771670ec9451
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-05-29 01:10:09 -0300

    Fix: Req5 and 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
-import Expenses from '../components/Expenses';
-import ExpensesTable from '../components/ExpensesTable';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <Header />
-        <Expenses />
-        <ExpensesTable />
-      </>);
+        <Form />
+      </div>);
   }
 }
 
